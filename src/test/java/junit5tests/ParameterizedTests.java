02509f1720f8e0d60071c2a86da95ecf68d0be8e
Randy Andujar <randujar.11@gmail.com>
2022-05-26 14:12:22 -0400

    Uso de #MethodSource

--- src/test/java/junit5tests/ParameterizedTests.java
+++ src/test/java/junit5tests/ParameterizedTests.java
@@ -2,10 +2,13 @@ package junit5tests;
 
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ParameterizedTests {
@@ -20,7 +23,7 @@ public class ParameterizedTests {
     @NullAndEmptySource
     @ValueSource(strings = {"firstString" , "secondString"})
     void stringValue(String theParam){
-        if (theParam != null && theParam != "") {
+        if (theParam != null && !theParam.equals("")) {
             System.out.println("thePar = " + theParam);
         } else System.out.println("Test fallido");
 
@@ -60,6 +63,38 @@ public class ParameterizedTests {
     void csvFileSource_StringDoubleIntString(String name, double price, int qty, String provider){
 
         System.out.println("name = " + name + ", price = " + price + ", qty = " + qty + ", provider = " + provider);
+    }
 
+    @ParameterizedTest
+    @MethodSource(value = "sourceString")
+    void methodSource_String(String param1){
+        System.out.println("param1 = " + param1);
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "sourceStringAsString")
+    void methodSource_StringStream(String param1){
+        System.out.println("param1 = " + param1);
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "sourceList_StringDouble")
+    void methodSource_StringDoubleList(String param1, double param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+
+    List<String> sourceString() {
+        return Arrays.asList("Tomate","Pepino","Lolito");
+    }
+
+    Stream<String> sourceStringAsString() {
+        return Stream.of("beetroot", "apple", "pear");
+    }
+
+    List<Arguments> sourceList_StringDouble() {
+        return Arrays.asList(arguments("Tomate", 2.0),
+                            arguments("Pepino",8.5),
+                            arguments("Lechuga", 7.8));
     }
 }
