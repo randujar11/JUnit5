25497b4903e5b2740fc40798d805c42a6ef0716b
Randy Andujar <randujar.11@gmail.com>
2022-05-26 13:15:16 -0400

    Parametros mandados desde CSV.

--- src/test/java/junit5tests/ParameterizedTests.java
+++ src/test/java/junit5tests/ParameterizedTests.java
@@ -2,6 +2,7 @@ package junit5tests;
 
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -46,5 +47,19 @@ public class ParameterizedTests {
     @CsvSource(value = {"Randy,29,true","Axel,1,false","Mariel,25,true"})
     void csvSource_StringIntBoolean(String param1, int param2, boolean param3){
         System.out.println("param1 = " + param1 + ", param2 = " + param2 + ", param3 = " + param3);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"Randy-Andujar", "Axel-Andujar"}, delimiter = '-')
+    void csvSource_StringWithDiffDelimiter(String param1, String param2){
+        System.out.println("param1 = " + param1 + ", param2 = " + param2);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/params/shoppinglist.csv", numLinesToSkip = 1)
+    void csvFileSource_StringDoubleIntString(String name, double price, int qty, String provider){
+
+        System.out.println("name = " + name + ", price = " + price + ", qty = " + qty + ", provider = " + provider);
+
     }
 }
